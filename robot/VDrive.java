1549f34340ed652e578a525c66d01908eb87b185
Nv7 <junk4Nv7@gmail.com>
2022-08-03 19:59:01 -0700

    Fix

--- robot/VDrive.java
+++ robot/VDrive.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,230 +11,229 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class VDrive {
-  ServerSocket serv;
-  Socket conn;
-  Thread thread;
-  Map<Key, Boolean> pressed;
-  boolean running;
+    ServerSocket serv;
+    Socket conn;
+    Thread thread;
+    Map<Key, Boolean> pressed;
+    boolean running;
 
-  public VDrive() {
-    pressed = new EnumMap<>(Key.class);
-  }
-
-  // Note: keybinds strings cant contain colon or semicolon
-  public void start(int port, Map<Key, String> keybinds) throws IOException {
-    serv = new ServerSocket(port);
-    System.out.println("Waiting for connection on port " + port + "...");
-    conn = serv.accept();
-
-    System.out.println("Received connection.");
-
-    // Generate keybind text
-    StringBuilder sb = new StringBuilder();
-    for (Map.Entry<Key, String> e : keybinds.entrySet()) {
-      sb.append(e.getKey().name() + ": " + e.getValue() + ";");
+    public VDrive() {
+        pressed = new EnumMap<>(Key.class);
     }
-    PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
-    out.println(sb.toString());
-    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-    // Start listening
-    thread = new Thread(new Runnable() {
-      public void run() {
-        try {
-          while (running) {
-            String[] line = in.readLine().split(":");
-            if (line == null) {
-              break;
-            }
-            Key key = Key.fromString(line[0]);
-            pressed.put(key, line[1].startsWith("true"));
-            String val = "Pressed";
-            if (!pressed.get(key)) {
-              val = "Released";
-            }
-            System.out.println(val + " " + line[0]);
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
+    // Note: keybinds strings cant contain colon or semicolon
+    public void start(int port, Map<Key, String> keybinds) throws IOException {
+        serv = new ServerSocket(port);
+        conn = serv.accept();
+
+        // Generate keybind text
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Key, String> e : keybinds.entrySet()) {
+            sb.append(e.getKey().name() + ": " + e.getValue() + ";");
         }
-      }
-    });
-    running = true;
-    thread.setName("VDrive");
-    thread.setPriority(Thread.NORM_PRIORITY);
-    thread.start();
-  }
+        PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
+        out.println(sb.toString());
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-  public void stop() throws IOException {
-    running = false;
-    conn.close();
-    serv.close();
-  }
-
-  public boolean isDown(Key key) {
-    return pressed.getOrDefault(key, false);
-  }
-
-  public enum Key {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    Q,
-    W,
-    E,
-    R,
-    T,
-    Y,
-    U,
-    I,
-    O,
-    P,
-    A,
-    S,
-    D,
-    F,
-    G,
-    H,
-    J,
-    K,
-    L,
-    Z,
-    X,
-    C,
-    V,
-    B,
-    N,
-    M,
-    COMMA,
-    PERIOD,
-    SLASH,
-    SEMICOLON,
-    APOSTROPHE,
-    LBRACKET,
-    RBRACKET,
-    MINUS,
-    EQUAL,
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    ZERO;
-
-    public String string() {
-      switch (this) {
-        case UP: return "UP";
-        case DOWN: return "DOWN";
-        case LEFT: return "LEFT";
-        case RIGHT: return "RIGHT";
-        case Q: return "Q";
-        case W: return "W";
-        case E: return "E";
-        case R: return "R";
-        case T: return "T";
-        case Y: return "Y";
-        case U: return "U";
-        case I: return "I";
-        case O: return "O";
-        case P: return "P";
-        case A: return "A";
-        case S: return "S";
-        case D: return "D";
-        case F: return "F";
-        case G: return "G";
-        case H: return "H";
-        case J: return "J";
-        case K: return "K";
-        case L: return "L";
-        case Z: return "Z";
-        case X: return "X";
-        case C: return "C";
-        case V: return "V";
-        case B: return "B";
-        case N: return "N";
-        case M: return "M";
-        case COMMA: return ",";
-        case PERIOD: return ".";
-        case SLASH: return "/";
-        case SEMICOLON: return ";";
-        case APOSTROPHE: return "'";
-        case LBRACKET: return "[";
-        case RBRACKET: return "]";
-        case MINUS: return "-";
-        case EQUAL: return "=";
-        case ONE: return "1";
-        case TWO: return "2";
-        case THREE: return "3";
-        case FOUR: return "4";
-        case FIVE: return "5";
-        case SIX: return "6";
-        case SEVEN: return "7";
-        case EIGHT: return "8";
-        case NINE: return "9";
-        case ZERO: return "0";
-        default: return "";
-      }
-    } 
-
-    public static Key fromString(String val) {
-      switch (val) {
-        case "UP": return UP;
-        case "DOWN": return DOWN;
-        case "LEFT": return LEFT;
-        case "RIGHT": return RIGHT;
-        case "Q": return Q;
-        case "W": return W;
-        case "E": return E;
-        case "R": return R;
-        case "T": return T;
-        case "Y": return Y;
-        case "U": return U;
-        case "I": return I;
-        case "O": return O;
-        case "P": return P;
-        case "A": return A;
-        case "S": return S;
-        case "D": return D;
-        case "F": return F;
-        case "G": return G;
-        case "H": return H;
-        case "J": return J;
-        case "K": return K;
-        case "L": return L;
-        case "Z": return Z;
-        case "X": return X;
-        case "C": return C;
-        case "V": return V;
-        case "B": return B;
-        case "N": return N;
-        case "M": return M;
-        case ",": return COMMA;
-        case ".": return PERIOD;
-        case "/": return SLASH;
-        case ";": return SEMICOLON;
-        case "'": return APOSTROPHE;
-        case "[": return LBRACKET;
-        case "]": return RBRACKET;
-        case "-": return MINUS;
-        case "=": return EQUAL;
-        case "1": return ONE;
-        case "2": return TWO;
-        case "3": return THREE;
-        case "4": return FOUR;
-        case "5": return FIVE;
-        case "6": return SIX;
-        case "7": return SEVEN;
-        case "8": return EIGHT;
-        case "9": return NINE;
-        case "0": return ZERO;
-        default: return null;
-      }
+        // Start listening
+        thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while (running) {
+                        String[] line = in.readLine().split(":");
+                        if (line == null) {
+                            break;
+                        }
+                        Key key = Key.fromString(line[0]);
+                        pressed.put(key, line[1].startsWith("true"));
+                        String val = "Pressed";
+                        if (!pressed.get(key)) {
+                            val = "Released";
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        running = true;
+        thread.setName("VDrive");
+        thread.setPriority(Thread.NORM_PRIORITY);
+        thread.start();
     }
-  }
+
+    public void stop() throws IOException {
+        running = false;
+        conn.close();
+        serv.close();
+    }
+
+    public boolean isDown(Key key) {
+        if (!pressed.containsKey(key)) {
+            return false;
+        }
+        return pressed.get(key);
+    }
+
+    public enum Key {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        Q,
+        W,
+        E,
+        R,
+        T,
+        Y,
+        U,
+        I,
+        O,
+        P,
+        A,
+        S,
+        D,
+        F,
+        G,
+        H,
+        J,
+        K,
+        L,
+        Z,
+        X,
+        C,
+        V,
+        B,
+        N,
+        M,
+        COMMA,
+        PERIOD,
+        SLASH,
+        SEMICOLON,
+        APOSTROPHE,
+        LBRACKET,
+        RBRACKET,
+        MINUS,
+        EQUAL,
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        ZERO;
+
+        public String string() {
+            switch (this) {
+                case UP: return "UP";
+                case DOWN: return "DOWN";
+                case LEFT: return "LEFT";
+                case RIGHT: return "RIGHT";
+                case Q: return "Q";
+                case W: return "W";
+                case E: return "E";
+                case R: return "R";
+                case T: return "T";
+                case Y: return "Y";
+                case U: return "U";
+                case I: return "I";
+                case O: return "O";
+                case P: return "P";
+                case A: return "A";
+                case S: return "S";
+                case D: return "D";
+                case F: return "F";
+                case G: return "G";
+                case H: return "H";
+                case J: return "J";
+                case K: return "K";
+                case L: return "L";
+                case Z: return "Z";
+                case X: return "X";
+                case C: return "C";
+                case V: return "V";
+                case B: return "B";
+                case N: return "N";
+                case M: return "M";
+                case COMMA: return ",";
+                case PERIOD: return ".";
+                case SLASH: return "/";
+                case SEMICOLON: return ";";
+                case APOSTROPHE: return "'";
+                case LBRACKET: return "[";
+                case RBRACKET: return "]";
+                case MINUS: return "-";
+                case EQUAL: return "=";
+                case ONE: return "1";
+                case TWO: return "2";
+                case THREE: return "3";
+                case FOUR: return "4";
+                case FIVE: return "5";
+                case SIX: return "6";
+                case SEVEN: return "7";
+                case EIGHT: return "8";
+                case NINE: return "9";
+                case ZERO: return "0";
+                default: return "";
+            }
+        }
+
+        public static Key fromString(String val) {
+            switch (val) {
+                case "UP": return UP;
+                case "DOWN": return DOWN;
+                case "LEFT": return LEFT;
+                case "RIGHT": return RIGHT;
+                case "Q": return Q;
+                case "W": return W;
+                case "E": return E;
+                case "R": return R;
+                case "T": return T;
+                case "Y": return Y;
+                case "U": return U;
+                case "I": return I;
+                case "O": return O;
+                case "P": return P;
+                case "A": return A;
+                case "S": return S;
+                case "D": return D;
+                case "F": return F;
+                case "G": return G;
+                case "H": return H;
+                case "J": return J;
+                case "K": return K;
+                case "L": return L;
+                case "Z": return Z;
+                case "X": return X;
+                case "C": return C;
+                case "V": return V;
+                case "B": return B;
+                case "N": return N;
+                case "M": return M;
+                case ",": return COMMA;
+                case ".": return PERIOD;
+                case "/": return SLASH;
+                case ";": return SEMICOLON;
+                case "'": return APOSTROPHE;
+                case "[": return LBRACKET;
+                case "]": return RBRACKET;
+                case "-": return MINUS;
+                case "=": return EQUAL;
+                case "1": return ONE;
+                case "2": return TWO;
+                case "3": return THREE;
+                case "4": return FOUR;
+                case "5": return FIVE;
+                case "6": return SIX;
+                case "7": return SEVEN;
+                case "8": return EIGHT;
+                case "9": return NINE;
+                case "0": return ZERO;
+                default: return null;
+            }
+        }
+    }
 }
